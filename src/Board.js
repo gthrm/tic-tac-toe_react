773fd0fc63b78fc7e987b31e893af2898a5ff29c
cdROma <truemrv@gmail.com>
2018-09-07 15:09:36 +0300

    что-то пошло не так

--- src/Board.js
+++ src/Board.js
@@ -1,36 +1,104 @@
 import React, { Component } from 'react'
 
-import Box from './Box'
+import Box from './Box.js'
+import Constants from './Constants.js'
 
 import './Board.css'
 
 class Board extends Component {
-
-    renderBox(i) {
-      return <Box id={i} />;
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      turn: Constants.X,
+      gameEnded: false,
+      winner: undefined
     }
-  
-    render() {
-  
-      return (
-        <div>
-          <div id="head">
-            <div className="headText">Game</div>
-          </div>
-          <div id="board">
-            {this.renderBox(0)}
-            {this.renderBox(1)}
-            {this.renderBox(2)}
-            {this.renderBox(3)}
-            {this.renderBox(4)}
-            {this.renderBox(5)}
-            {this.renderBox(6)}
-            {this.renderBox(7)}
-            {this.renderBox(8)}
-          </div>
-        </div>
-      );
+
+    this.gameState = {
+      board: Array(9).fill(''),
+      totalMov: 0
     }
   }
 
-  export default Board
+  renderBox(i) {
+    return <Box id={i} />;
+  }
+
+  clicked(event) {
+
+    if (this.gameState.board[event.target.dataset.box] == '') {
+      this.gameState.board[event.target.dataset.box] = this.gameState.turn == Constants.X ? 'X' : 'O' 
+      event.target.innerHTML = this.state.turn;
+      this.setState ({
+        turn: this.state.turn == Constants.X ? Constants.O : Constants.X,
+      })
+    }
+
+    let result = this.check()
+
+    if (result == 'X') {
+      this.setState({
+        gameEnded: true,
+        winner: 'X'
+      })
+      console.log('====================================');
+      console.log('winner X');
+      console.log('====================================');
+    } else if (result == 'O') {
+      this.setState({
+        gameEnded: true,
+        winner: 'O'
+      })
+      console.log('====================================');
+      console.log('winner O');
+      console.log('====================================');
+    } else if (result == 'draw') {
+      this.setState({
+        gameEnded: true,
+        winner: 'draw'
+      })
+    }
+  }
+
+  check() {
+    let mov = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    let board = this.gameState.board
+    for (let i = 0; i<mov.length; i++) {
+      
+      if (board[mov[i][0]] == board[mov[i][1]] && board[mov[i][1]] == board[mov[i][2]]) {
+        return board[mov[i][0]]
+      }
+      console.log(this.gameState.totalMov);
+      
+      if (this.gameState.totalMov == 9) {
+        return 'draw'
+      }
+
+    }
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div id="head">
+          <div className="headText">Выиграл {this.state.winner}</div>
+        </div>
+        <div id="board" onClick={(e) => this.clicked(e)}>
+          {this.renderBox(0)}
+          {this.renderBox(1)}
+          {this.renderBox(2)}
+          {this.renderBox(3)}
+          {this.renderBox(4)}
+          {this.renderBox(5)}
+          {this.renderBox(6)}
+          {this.renderBox(7)}
+          {this.renderBox(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board
---
+++ src/Constants.js
@@ -0,0 +1,4 @@
+export default {
+    X:'<svg id=""\nxmlns="http://www.w3.org/2000/svg"\nxmlns:xlink="http://www.w3.org/1999/xlink"\nwidth="88px" height="88px">\n<path fill-rule="evenodd"  stroke="rgb(255, 252, 252)" stroke-width="0px" stroke-linecap="butt" stroke-linejoin="miter" fill="#ffffff"\nd="M77.536,70.464 L70.464,77.536 L41.500,48.571 L12.536,77.536 L5.464,70.464 L34.429,41.500 L5.464,12.536 L12.536,5.464 L41.500,34.429 L70.464,5.464 L77.536,12.536 L48.571,41.500 L77.536,70.464 Z"></path>\n</svg>',
+    O: '<svg id=""\nxmlns="http://www.w3.org/2000/svg"\nxmlns:xlink="http://www.w3.org/1999/xlink"\nwidth="78px" height="78px">\n<path fill-rule="evenodd"  stroke="rgb(255, 252, 252)" stroke-width="10px" stroke-linecap="butt" stroke-linejoin="miter" fill="none"\nd="M36.500,5.000 C53.897,5.000 68.000,19.103 68.000,36.500 C68.000,53.897 53.897,68.000 36.500,68.000 C19.103,68.000 5.000,53.897 5.000,36.500 C5.000,19.103 19.103,5.000 36.500,5.000 Z"></path>\n</svg>'
+}
