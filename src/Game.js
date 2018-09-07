1159186d12b8a0e163d1260b35817beaa2552ee1
cdROma <truemrv@gmail.com>
2018-09-07 20:55:39 +0300

    button start-reset

--- src/Game.js
+++ src/Game.js
@@ -5,14 +5,29 @@ import Board from './Board.js'
 // import './Game.css'
 
 class Game extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      game: false
+    }
+  }
+onGame(){
+  console.log(this);
+  this.setState({
+    game: this.state.game == false ? true : false
+  });
+}
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+         {this.state.game == true? <Board />:null}
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>
+            <button id="start" onClick={(e) => this.onGame(e.target)}>Reset</button>
+          </div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
