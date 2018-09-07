9a4cf0d1a9fdbe9d3a143cd53c8f36db664dfc83
cdROma <truemrv@gmail.com>
2018-09-07 15:59:44 +0300

    2 users

--- src/Board.js
+++ src/Board.js
@@ -12,7 +12,8 @@ class Board extends Component {
     this.state = {
       turn: Constants.X,
       gameEnded: false,
-      winner: undefined
+      winner: undefined,
+      wList: ''
     }
 
     this.gameState = {
@@ -47,36 +48,41 @@ class Board extends Component {
       console.log('====================================');
       console.log('winner X');
       console.log('====================================');
+      return false
     } else if (result == 'O') {
       this.setState({
         gameEnded: true,
-        winner: 'O'
+        winner: 'O',
+        wList: 'Выиграли нолики'
       })
       console.log('====================================');
       console.log('winner O');
       console.log('====================================');
+      return false
     } else if (result == 'draw') {
       this.setState({
         gameEnded: true,
-        winner: 'draw'
+        winner: 'draw',
+        wList: 'Ничья!'
       })
       console.log('====================================');
       console.log('draw');
       console.log('====================================');
+    } else {
+      return false
     }
   }
 
   check() {
     var mov = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]]
     var board = this.gameState.board
-    for (let i = 0; i<mov.length; i++) {
-      
+    for (let i = 0; i<mov.length; i++) {      
       if (board[mov[i][0]] == board[mov[i][1]] && board[mov[i][1]] == board[mov[i][2]]) {
         return board[mov[i][0]]
-      }
-      
-      if (this.gameState.totalMov == 9) {
+      } else if (this.gameState.totalMov == 9) {
         return 'draw'
+      } else {
+        return false
       }
 
     }
@@ -87,7 +93,7 @@ class Board extends Component {
     return (
       <div>
         <div id="head">
-          <div className="headText">Выиграл {this.state.winner}</div>
+          <div className="headText">{this.state.wList}</div>
         </div>
         <div id="board" onClick={(e) => this.clicked(e)}>
           {this.renderBox(0)}
